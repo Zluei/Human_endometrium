d6f7f9b65790c32e3145d172b5e8da1255b56b71
Zluei <112161731+Zluei@users.noreply.github.com>
2024-12-24 16:09:38 +0800

    Add files via upload

---
+++ cellphonedb_step2.sh
@@ -0,0 +1,13 @@
+{\rtf1\ansi\ansicpg936\cocoartf2820
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset134 STSongti-SC-Regular;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww16520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\li720\fi-720\partightenfactor0
+
+\f0\fs32 \cf0 \expnd0\expndtw0\kerning0
+cellphonedb method statistical_analysis LH9_meta.txt LH9_count.txt --counts-data hgnc_symbol\
+\pard\pardeftab720\partightenfactor0
+\cf0 cellphonedb plot heatmap_plot LH9_meta.txt --pvalues-path out/pvalues.txt --output-path plot\
+}
